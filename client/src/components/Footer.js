0cb1b22c2fa58cebf6dcbb86f226db1b3bfc80c1
Kirtan <kirtanp8@icloud.com>
2022-01-02 10:45:49 +0000

    going to deploy

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,7 +8,9 @@ const Footer = () => {
         <a href='https://github.com/kirtanp8' target='_blank' rel='noreferrer'>
           <FaGithub className='icon-github' />
         </a>
-        <p>Created by Kirtan Patel</p>
+        <div className='take-space-footer'></div>
+        <p>Created by Kirtan Patel  </p>
+        <div className='take-space-footer'></div>
         <a href='https://www.linkedin.com/in/kirtanp8/' target='_blank' rel='noreferrer'>
           <FaLinkedin className='icon-linkedin' />
         </a>

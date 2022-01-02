5d8cf85ecd56f922b2494b1177eb2107796a422f
Kirtan <kirtanp8@icloud.com>
2022-01-02 15:07:59 +0000

    ready to deploy

--- SpiritsOClock/SpiritsOClock/urls.py
+++ SpiritsOClock/SpiritsOClock/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/reviews/', include('reviews.urls')),
     path('api/recipes/', include('recipes.urls')),
-    path('api/auth/', include('jwt_auth.urls'))
+    path('api/auth/', include('jwt_auth.urls')),
+    re_path(r'^.*$', index)
 ]
